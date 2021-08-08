b7cf52ee86631179e31c44c040af9c2bea734252
AlgoFilipino <88389066+AlgoFilipino@users.noreply.github.com>
2021-08-08 15:29:50 +0800

    algodded fonts and set-up

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+  <script>
+    <div className="App">
+      <header className="App-header">
+        <div class= "navbar"> 
+         <div class="Banner" src='assets'>
+         </div>
+        </div>
+      </header>
+    </div>
+    </script>
+  );
+}
+
+export default App;
