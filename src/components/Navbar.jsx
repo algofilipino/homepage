78fc5bfbc5a74f2672afb9b7cbfcf672860e42e1
Felix Jerome Dela Fuente <dlfuente.felixjerome@gmail.com>
2021-09-10 17:13:30 +0800

    fixed tailwind config and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,62 +11,68 @@ function Navbar() {
 
   return (
     <>
-      <nav className="navbar bg-transparent h-16 text-btn flex justify-center items-center sticky z-10 pt-16 pl-20 pr-20 font-josefin">
-        <div className="navbar-container flex justify-center items-center w-11/12">
-          <Link
-            to="/"
-            className="navbar-logo w-24 justify-self-start flex items-center"
-          >
-            <img src={logoTitle} alt="Algo Logo" />
-          </Link>
-          <h1 className="text-p">algo filipino</h1>
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+      <nav className="navbar bg-transparent text-btn flex justify-center items-center z-10 mt-16 font-josefin">
+        <div className="navbar-container flex justify-between items-center w-10/12 mx-auto fixed">
+          <div className="flex">
+            <Link
+              to="/"
+              className="navbar-logo w-20 justify-self-start flex items-center ml-6 mr-6 mb-2"
+            >
+              <img src={logoTitle} alt="Algo Logo" />
+            </Link>
+            <h1 className="text-name w-4/12b flex items-center">
+              algo filipino
+            </h1>
+            <div className="menu-icon" onClick={handleClick}>
+              <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+            </div>
           </div>
-          <ul
-            className={
-              click
-                ? "nav-menu active"
-                : "nav-menu auto-rows-auto text-center flex justify-end w-10/12 tracking-widest"
-            }
-          >
-            <li className="nav-item">
-              <Link
-                to="/"
-                className="nav-links text-black flex items-center pl-6 pr-6 text-p"
-                onClick={closeMobileMenu}
-              >
-                HOME
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/learn"
-                className="nav-links text-black flex items-center pl-6 pr-6"
-                onClick={closeMobileMenu}
-              >
-                ABOUT
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/translate"
-                className="nav-links text-black flex items-center pl-6 pr-6"
-                onClick={closeMobileMenu}
-              >
-                EVENTS
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/about"
-                className="nav-links text-black flex items-center pl-6 pr-6"
-                onClick={closeMobileMenu}
-              >
-                CONTACT
-              </Link>
-            </li>
-          </ul>
+          <div>
+            <ul
+              className={
+                click
+                  ? "nav-menu active"
+                  : "nav-menu auto-rows-auto text-center flex justify-end tracking-widest"
+              }
+            >
+              <li className="nav-item">
+                <Link
+                  to="/"
+                  className="nav-links flex items-center px-5 transition text-black hover:text-yellow-100 hover:"
+                  onClick={closeMobileMenu}
+                >
+                  HOME
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/learn"
+                  className="nav-links flex items-center px-5 transition text-black hover:text-yellow-100 hover:"
+                  onClick={closeMobileMenu}
+                >
+                  ABOUT
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/translate"
+                  className="nav-links flex items-center px-5 transition text-black hover:text-yellow-100 hover:"
+                  onClick={closeMobileMenu}
+                >
+                  EVENTS
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/about"
+                  className="nav-links flex items-center px-5 transition text-black hover:text-yellow-100 hover:"
+                  onClick={closeMobileMenu}
+                >
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
